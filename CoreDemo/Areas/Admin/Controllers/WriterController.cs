5ad08e55a2052a730306919d855a078e9cde54db
beyzakalci <beyza.kalci@hotmail.com>
2022-09-01 23:45:05 +0300

    .

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -21,11 +21,36 @@ namespace CoreDemo.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        
         public IActionResult GetWriterByID(int writerid)
         {
             var findWriter = writers.FirstOrDefault(x => x.Id==writerid);
             var jsonWriters = JsonConvert.SerializeObject(findWriter);
             return Json(jsonWriters);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonwriters = JsonConvert.SerializeObject(w);
+            return Json(jsonwriters);
+        }
+
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
 
         }
         public static List<WriterClass> writers = new List<WriterClass>
