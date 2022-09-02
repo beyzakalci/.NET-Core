dc9ba5a8514984062467374b9c5350eacf578ad4
beyzakalci <beyza.kalci@hotmail.com>
2022-09-02 17:50:00 +0300

    .

--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,9 +17,14 @@ namespace CoreDemo.ViewComponents.Writer
 
         Context c = new Context();
 
+       
+
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var username = User.Identity.Name;
+            ViewBag.v = username;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = writermanager.GetWriterById(writerID);
             return View(values);
